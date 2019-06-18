0c9e4a40eb7ebead73152c1cc3ab0287d94c1d38
Owen Melbourne <owenmelbz@gmail.com>
2019-06-19 00:34:46 +0100

    🚀 Documentation updated

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,13 +10,6 @@ module.exports = {
         plugins: [
             require('tailwindcss'),
             require('autoprefixer'),
-            process.env.NODE_ENV === 'production' && purgecss({
-                content: glob.sync([
-                    path.join(__dirname, './components/**/*.{vue,js,php,html}'),
-                    path.join(__dirname, '../node_modules/@vuepress/theme-default/**/*.{vue,js,php,html}'),
-                ]),
-                defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-            })
         ].filter(i => i)
     },
     themeConfig: {
